9b1cbb1f567c616965e06003134240e048b9c254
Pytten <bjarnedeinarsen@gmail.com>
2026-08-24 16:01:26 +0200

    kjerneoppgaver lab 1

---
+++ Lab 1/distance.py
@@ -0,0 +1,15 @@
+
+x1 = float(input())
+print(f"x1 is {x1}")
+y1 = float(input())
+print(f"y1 is {y1}")
+x2 = float(input())
+print(f"x2 is {x2}")
+y2 = float(input())
+print(f"y2 is {y2}")
+
+x = (x2-x1)**0.5
+y = (y2-y1)**0.5
+avstand = x + y
+
+print(f"avstanden mellom de to punktene er: {avstand}")
---
+++ Lab 1/semester_budget.py
@@ -0,0 +1,22 @@
+
+# Hva er budsjettet ditt?
+# 7000
+# Hvor mye bruker du på bolig?
+# 3500
+# Hvor mye bruker du på mat?
+# 3000
+# Det er 500 NOK igjen, det er nok til 11 kopper kaffe!
+print("hva er budsjettet ditt?")
+budsjett = int(input())
+
+print("Hvor mye bruker du på bolig?")
+bolig = int(input())
+
+print("Hvor mye bruker du på mat?")
+mat = int(input())
+
+kaffe_budsjett = budsjett - bolig -mat
+kaffe_pris = 500/11
+kopper_kaffe = kaffe_budsjett/kaffe_pris
+
+print(f"Det er {kaffe_budsjett} NOK igjen, det er nok til {kopper_kaffe} kopper kaffe")
