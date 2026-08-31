da502306930d024a63dfcd23cba93069a2418356
Pytten <bjarnedeinarsen@gmail.com>
2026-08-31 09:58:47 +0200

    fikset formelen for distanse

--- Lab 1/distance.py
+++ Lab 1/distance.py
@@ -8,8 +8,8 @@ print(f"x2 is {x2}")
 y2 = float(input())
 print(f"y2 is {y2}")
 
-x = (x2-x1)**0.5
-y = (y2-y1)**0.5
-avstand = x + y
+x = (x2-x1)**2
+y = (y2-y1)**2
+avstand = (x + y)**0.5
 
 print(f"avstanden mellom de to punktene er: {avstand}")
