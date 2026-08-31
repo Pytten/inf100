29cd21b865d403c07cce27de9a24c44b585fa5b8
Pytten <bjarnedeinarsen@gmail.com>
2026-08-31 13:56:50 +0200

    Finne to farger

---
+++ Lab 1/fargemix.py
@@ -0,0 +1,20 @@
+
+
+colA = int(input())
+colA_B = colA%1000
+colA_G = round(((colA-colA_B)%1000000)/1000)
+colA_R = round(((colA-colA_B-colA_G)%1000000000)/1000000)
+
+colB = int(input())
+colB_B = colB%1000
+colB_G = round(((colB-colB_B)%1000000)/1000)
+colB_R = round(((colB-colB_B-colB_G)%1000000000)/1000000)
+
+rationB = float(input())
+
+R = round((1-rationB)*colA_R + rationB*colB_R)
+G = round((1-rationB)*colA_G + rationB*colB_G)
+B = round((1-rationB)*colA_B + rationB*colB_B)
+
+RGB = R*1000000 + G*1000 + B
+print(RGB)
